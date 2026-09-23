e1c9accc60d0a4acca4dd1d043f6e5f6f2265de5
Mrpsousa <urameshi.uba@gmail.com>
2026-09-23 11:31:21 -0300

    feat: maven start project

---
+++ src/test/java/com/vr/mini_authorizer/MiniAuthorizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vr.mini_authorizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniAuthorizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
